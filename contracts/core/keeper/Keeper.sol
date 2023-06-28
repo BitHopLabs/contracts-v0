2d0e0708cb45c8120b7869f50735743f8015051b
michaelach302 <michaelahl302@gmail.com>
2023-06-28 22:27:04 +0800

    feature: minor update

--- contracts/core/keeper/Keeper.sol
+++ contracts/core/keeper/Keeper.sol
@@ -4,13 +4,18 @@ pragma solidity ^0.8.0;
 import "../../interface/IKeeper.sol";
 import "../AbstractAccount.sol";
 import "./AAStorage.sol";
+import "../../libraries/Decoded.sol";
 
 contract Keeper is IKeeper, AAStorage {
 
     function execute(
         bytes calldata data
     ) external override returns (bool) {
-        require(verify(data), "E0");
+        bytes memory payload = Decoded.verify(data);
+        (uint8 size, bytes[] memory callData) = Decoded.decodePayload(payload);
+        for (uint8 i = 0; i < size; i++) {
+
+        }
         return true;
     }
 
@@ -18,16 +23,10 @@ contract Keeper is IKeeper, AAStorage {
         bytes calldata data
     ) external {
         address newAA = address(new AbstractAccount(address(this)));
-        require(verify(data), "E0");
         require(owner[newAA] == address(0), "E0");
         renewOwnership(newAA, newAA);
     }
 
-    function verify(
-        bytes calldata data
-    ) internal returns (bool) {
-        return true;
-    }
 
     function renewOwnership(
         address eoa,
--- contracts/libraries/Decoded.sol
+++ contracts/libraries/Decoded.sol
@@ -5,12 +5,16 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 library Decoded {
 
-    function verify(address eoa, uint orderId, bytes calldata payload, bytes calldata signature) external pure {
-        bytes32 digest = keccak256(abi.encodePacked(
+    function verify(bytes calldata raw) external view returns (bytes memory payload) {
+        (address eoa, uint orderId, bytes memory signature, bytes memory payload) = decodeRaw(raw);
+        (,uint dstChain,uint expTime) = decodeOrderId(orderId);
+        require(expTime < block.timestamp, "E0");
+        require(dstChain == block.chainid, "E0");
+        require(eoa == ECDSA.recover(keccak256(abi.encodePacked(
             orderId,
             payload
-        ));
-        require(eoa == ECDSA.recover(digest, signature), "E1");
+        )), signature), "E0");
+        return payload;
     }
 
     function genOrderId(
@@ -25,13 +29,13 @@ library Decoded {
         expTime = (orderId >> 64) & ((1 << 32) - 1);
     }
 
-    // eoa、orderId、signature、payload
+    // eoa、orderId、signature、payload transferInfo
     function decodeRaw(bytes calldata raw) public pure returns (address eoa, uint orderId, bytes memory signature, bytes memory payload) {
         (eoa, orderId, signature, payload) = abi.decode(raw, (address, uint, bytes, bytes));
     }
 
     // size, len0, len1..., contract0、data0、contract1、data1...
-    function decodePayload(bytes calldata payload) public pure returns (bytes[] memory callData){
+    function decodePayload(bytes calldata payload) public pure returns (uint8 size, bytes[] memory callData){
         (uint8 size, uint16[] memory lens) = abi.decode(payload[: 352], (uint8, uint16[]));
         uint begin = 352;
         bytes[] memory callData = new bytes[](size);
@@ -40,6 +44,6 @@ library Decoded {
             begin = begin + lens[i];
         }
 
-        return callData;
+        return (size, callData);
     }
 }
